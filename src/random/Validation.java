7812be9dc7f286c035966c958a6d762d879e4727
League Student <league@iMac-22.attlocal.net>
2020-02-26 19:56:42 -0800

    valadation and dice roll

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -12,14 +12,34 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
 		
-		System.out.println(randomNumber);
+		
+		
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+for (int i = 0; i < 10; i++) {
+	
+	int randomNumber = randomMaker.nextInt(5);
+	System.out.println(randomNumber);
+	
+		if(randomNumber==0) {
+	JOptionPane.showMessageDialog(null, "u are nice");
+}
+if(randomNumber==1) {
+	JOptionPane.showMessageDialog(null, "u are good at coding");
+}
+if(randomNumber==2) {
+	JOptionPane.showMessageDialog(null, "u are cool");
+}
+if(randomNumber==3) {
+	JOptionPane.showMessageDialog(null, "u are pretty");
+}
+if(randomNumber==4) {
+	JOptionPane.showMessageDialog(null, "u are a cool kid");
+}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
+}
 }
